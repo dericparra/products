ea1a85e824985c1174d3a53ac7e119fab145ea8c
dericparra <parradeko@gmail.com>
2019-06-21 09:15:52 -0300

    first js

--- b/main.js
+++ b/main.js
@@ -1,3 +1,19 @@
+var white = document.querySelector ("[value='white']")
+var green = document.querySelector ("[value='green']")
+var purple = document.querySelector("[value='purple']")
+var yellow = document.querySelector("[value='yellow']")
+var black = document.querySelector ("[value='black']")
+white = 30
+green = 5  
+purple = 80 
+yellow = 10 
+black = 100 
+console.log(white)
+console.log(green)
+console.log(purple)
+console.log(yellow)
+console.log(black)
+
 var button = document.querySelector("button")
 console.log(button)
 button.addEventListener("click", function() {
