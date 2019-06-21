d6c047d0d744d548123e3cbff27732576c29d1a8
dericparra <parradeko@gmail.com>
2019-06-21 09:49:27 -0300

    js 2

--- b/main.js
+++ b/main.js
@@ -3,16 +3,15 @@ var green = document.querySelector ("[value='green']")
 var purple = document.querySelector("[value='purple']")
 var yellow = document.querySelector("[value='yellow']")
 var black = document.querySelector ("[value='black']")
+
 white = 30
 green = 5  
 purple = 80 
 yellow = 10 
 black = 100 
-console.log(white)
-console.log(green)
-console.log(purple)
-console.log(yellow)
-console.log(black)
+
+var input = document.querySelector ("[data-Input='input']")
+console.log(input)
 
 var button = document.querySelector("button")
 console.log(button)
